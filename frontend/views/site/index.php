d513eea3f83353c2b8bb11373780accbb330de2d
Yasrul <yasrul@gmail.com>
2015-07-28 04:58:24 +0800

    2015-07-28 04:58

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -52,4 +52,73 @@ $this->title = 'Yii 2 Build';
         ]);
         Modal::end();
     ?>
+    <br/>
+    <br/>
+    <?php
+        echo Alert::widget([
+            'options' => ['class' => 'alert-info'],
+            'body' => 'Launch your project like rocket',
+        ]);
+    ?>
+    <div class="body-content">
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Free</h2>
+                <p>
+                    <?php
+                    if(!Yii::$app->user->isGuest) {
+                        echo Yii::$app->user->identity->username. ' is doing cool stuff';
+                    }?>
+                    Starting with this free, open source Yii 2 template and it will save you
+                    a lot of time. You can deliver projects to the customer quickly, with
+                    a lot of boilerplate already taken care of for you, so you can concentrate
+                    on the complicated stuff.
+                </p>
+                <p>
+                    <a class="btn btn-default" href="http://www.yiiframework.com/doc-2.0/guide-index.html">
+                        Yii Documentation &raquo;
+                    </a>
+                </p>
+                <?php
+                echo FacebookPlugin::widget([
+                    'type' => FacebookPlugin::LIKE,
+                    'settings' => [],
+                ]);
+                ?>
+            </div>
+            <div class="col-lg-4">
+                <h2>Advantages</h2>
+                <p>
+                    Ease of use is a huge advantage. We've simplifiled RBAC and given you Free/Paid
+                    user type out of the box. The Social plugins are so quick and easy to install,
+                    you will love it!
+                </p>
+                <p>
+                    <a class="btn btn-default" href="http://www.yiiframework.com/forum/">
+                    Yii Forum &raquo;
+                    </a>
+                </p>
+                <?php
+                echo FacebookPlugin::widget([
+                    'type' => FacebookPlugin::COMMENT,
+                    'settings' => ['href' => 'http://localhost/yii2build/frontend/web', 'width' => '350']
+                ]);
+                ?>
+            </div>
+            <div class="col-lg-4">
+                <h2>Code Quick, Code Right !</h2>
+                <p>
+                    Leverage the power of the awesome Yii 2 framework with this enhanced template.
+                    Based Yii 2's advanced template, you get a full frontend and backend
+                    implementation that features rich UI for backend management.
+                </p>
+                <p>
+                    <a class="btn btn-default" href="http://www.yiiframework.com/extensions/">
+                        Yii Extensions &raquo;
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+    
 </div>
